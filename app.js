d57f28554191e2baf524686a7ac28ed4318a1527
Türkcan Yılmaz <turkcanyilmaz8@gmail.com>
2023-08-05 20:38:59 +0300

    update color generation

--- app.js
+++ app.js
@@ -1,80 +1,152 @@
-const randomColor = () => {
-    const r = Math.floor((Math.random() * 255 + 1))
-    const g = Math.floor((Math.random() * 255 + 1))
-    const b = Math.floor((Math.random() * 255 + 1))
-    return `rgb(${r},${g},${b})`;
-}
 
 const title = document.querySelector("h1");
 const midSection = document.querySelectorAll(".middle-section button");
-const buttons = document.querySelectorAll(".bottom-section button")
-const mediumButtons = document.querySelectorAll(".bottom-section .middle-row button")
-const hardButtons = document.querySelectorAll(".bottom-section .bottom-row button")
+const buttons = document.querySelectorAll(".bottom-section .color-btn");
+const easyButtons = document.querySelectorAll(".bottom-section .top-row .color-btn");
+const mediumButtons = document.querySelectorAll(".bottom-section .middle-row .color-btn");
+const hardButtons = document.querySelectorAll(".bottom-section .bottom-row .color-btn");  
 const easyMode = document.querySelector(".middle-section .game-modes #easy-btn")
 const mediumMode = document.querySelector(".middle-section .game-modes #medium-btn")
 const hardMode = document.querySelector(".middle-section .game-modes #hard-btn")
+const resetGame = document.querySelector(".middle-section .reset-game button")
 
+const randomColor = () => {
+  const r = Math.floor((Math.random() * 255 + 1))
+  const g = Math.floor((Math.random() * 255 + 1))
+  const b = Math.floor((Math.random() * 255 + 1))
+  return `rgb(${r},${g},${b})`;
+}
 
-midSection.forEach((button) => {
-    title.innerText = randomColor().toUpperCase();
-    button.addEventListener("click", function() {
-        title.innerText = randomColor().toUpperCase();
-        buttons.forEach((button) => {
-            button.style.backgroundColor = randomColor();
-        });
-    });
+const correctColor = () => {
+  return randomColor();
+}
+
+const generateColorsForEasy = () => {
+  const correctButtonIndex = Math.floor(Math.random() * 3);
+  const otherColors = [];
+
+  for (let i = 0; i < buttons.length; i++) {
+    if (i === correctButtonIndex) {
+      const correctColorValue = correctColor();
+      buttons[i].style.backgroundColor = correctColorValue;
+      title.innerText = correctColorValue.toUpperCase();
+    } else {
+      let color = randomColor();
+      while (otherColors.includes(color)) {
+        color = randomColor();
+      }
+      buttons[i].style.backgroundColor = color;
+      otherColors.push(color);
+    }
+  }
+};
+
+const generateColorsForMedium = () => {
+  const correctButtonIndex = Math.floor(Math.random() * 6);
+  const otherColors = [];
+
+  for (let i = 0; i < buttons.length; i++) {
+    if (i === correctButtonIndex) {
+      const correctColorValue = correctColor();
+      buttons[i].style.backgroundColor = correctColorValue;
+      title.innerText = correctColorValue.toUpperCase();
+    } else {
+      let color = randomColor();
+      while (otherColors.includes(color)) {
+        color = randomColor();
+      }
+      buttons[i].style.backgroundColor = color;
+      otherColors.push(color);
+    }
+  }
+};
+
+const generateColorsForHard = () => {
+  const correctButtonIndex = Math.floor(Math.random() * 9);
+  const otherColors = [];
+
+  for (let i = 0; i < buttons.length; i++) {
+    if (i === correctButtonIndex) {
+      const correctColorValue = correctColor();
+      buttons[i].style.backgroundColor = correctColorValue;
+      title.innerText = correctColorValue.toUpperCase();
+    } else {
+      let color = randomColor();
+      while (otherColors.includes(color)) {
+        color = randomColor();
+      }
+      buttons[i].style.backgroundColor = color;
+      otherColors.push(color);
+    }
+  }
+};
+
+generateColorsForEasy();
+
+  resetGame.addEventListener("click", function() {
+    generateColorsForEasy();
   });
 
-buttons.forEach((button) => {
-    button.style.backgroundColor = randomColor();
-})
-
-mediumButtons.forEach((button) => {
+  mediumButtons.forEach((button) => {
     button.style.display = "none";
-})
-
-hardButtons.forEach((button) => {
+  });
+  hardButtons.forEach((button) => {
     button.style.display = "none";
-})
+  });  
 
-easyMode.addEventListener("click", function () {
+easyMode.addEventListener("click", function() {
     mediumButtons.forEach((button) => {
-        button.style.display = "none";
-    })
-    hardButtons.forEach((button) => {
-        button.style.display = "none";
-    })
-})
-
-mediumMode.addEventListener("click", function () {
-    mediumButtons.forEach((button) => {
-        button.style.display = "flex";
+      button.style.display = "none";
     });
     hardButtons.forEach((button) => {
-        button.style.display = "none";
-    })
-});
+      button.style.display = "none";
+    });
+    easyMode.style.backgroundColor = "#496ba3";
+    easyMode.style.color = "#faf8f1";
+    mediumMode.style.backgroundColor = "#faf8f1";
+    mediumMode.style.color = "#496ba3";
+    hardMode.style.backgroundColor = "#faf8f1";
+    hardMode.style.color = "#496ba3";
+    generateColorsForEasy();
 
-hardMode.addEventListener("click", function () {
+  });
+  
+  mediumMode.addEventListener("click", function() {
     mediumButtons.forEach((button) => {
-        button.style.display = "flex";
+      button.style.display = "flex";
     });
     hardButtons.forEach((button) => {
-        button.style.display = "flex";
+      button.style.display = "none";
     });
-});
+    easyMode.style.backgroundColor = "#faf8f1";
+    easyMode.style.color = "#496ba3";
+    mediumMode.style.backgroundColor = "#496ba3";
+    mediumMode.style.color = "#faf8f1";
+    hardMode.style.backgroundColor = "#faf8f1";
+    hardMode.style.color = "#496ba3";
+    generateColorsForMedium();
 
-
-/* mediumMode.addEventListener("click", function () {
+    resetGame.addEventListener("click", function() {
+      generateColorsForMedium();
+    });
+  });
+  
+  hardMode.addEventListener("click", function() {
     mediumButtons.forEach((button) => {
-        button.style.display = "flex";
-    })
-})
-
-hardMode.addEventListener("click", function () {
+      button.style.display = "flex";
+    });
     hardButtons.forEach((button) => {
-        button.style.display = "flex";
-    })
-}) */
+      button.style.display = "flex";
+    });
+    easyMode.style.backgroundColor = "#faf8f1";
+    easyMode.style.color = "#496ba3";
+    mediumMode.style.backgroundColor = "#faf8f1";
+    mediumMode.style.color = "#496ba3";
+    hardMode.style.backgroundColor = "#496ba3";
+    hardMode.style.color = "#faf8f1";
+    generateColorsForHard();
 
-
+    resetGame.addEventListener("click", function() {
+      generateColorsForHard();
+    });
+  });
